dd34bd5d9567f7114545741c934fa706e4789342
Chloe905 <cchloehsieh@gmail.com>
2022-11-21 21:20:04 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require('express')
-
+const mongoose = require('mongoose')
 const port = 3000
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const app = express()
+
+mongoose.connect(process.env.MONGODB_URI)
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('Mongodb error')
+})
+db.once('open', () => {
+  console.log('Mongodb connected')
+})
 
 app.get('/', (req, res) => {
   res.send('This page is working sucessful')
